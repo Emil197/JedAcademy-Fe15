8bca880f903a63a945ec73daa39fabe0ca676f30
Emil197 <qasimzadeemil71@gmail.com>
2023-06-10 17:18:03 +0400

    tasks js 1
    task js

---
+++ Tasks js 1/app.js
@@ -0,0 +1,139 @@
+
+//  Task -01
+
+// alert("My Name Is Emil")
+
+
+
+// Task -02
+// const name="Emil";
+// console.log(name);
+
+// Task -03
+
+// person,c@art,
+
+
+// Task -04
+
+// let bookprice=14
+// let bookamount=750
+// let netice=(bookamount*bookprice)
+// console.log(netice);
+
+// Task -05
+
+// let kmspeed=75
+// let mspeed=(kmspeed*1000)
+// console.log(mspeed)
+
+
+// Task -06
+// let birthday=(prompt("Dogum ilinizi Qeyd Edin"))
+// let now=(prompt("indiki ili qeyd edin"))
+// let days=((now-birthday)*365)
+// console.log(days)
+
+// Task -07
+// let num=11
+// console.log(11**5,11%3);
+
+// Task -08
+
+// let firstname="Emil"
+// let lastname="Qasimzade"
+
+// console.log("Sizin adiniz",firstname,lastname);
+
+// console.log("Tanisligimiza Cox Sadam");
+
+
+// Task -10
+ 
+// let fullname="Emil Qasimzade"
+
+// console.log(fullname.length);
+
+
+// Task -11
+
+// let fullname="Emil Qasinmzade"
+
+// console.log(fullname.charAt(0));
+
+
+// Task -12
+
+// prompt("Ad ve soyad yaz")
+// console.log(prompt("Soyad ve ad"));
+
+// Task -13
+// let name="Emil"
+// console.log("Salam",name);
+// console.log(prompt("Ad Yaz"));
+// console.log(("Salam Emil"));
+
+
+// Task -14
+// let number1=12
+// let number2=3
+// console.log(number1+number2);
+// console.log(number1-number2);
+// console.log(number1*number2);
+// console.log(number1/number2);
+
+
+// let n1=Number(prompt("Birinci ededi daxil edin"))
+// let n2=Number(prompt("Ikinci ededi daxil edin"))
+// console.log(n1+n2);
+// console.log(n1-n2);
+// console.log(n1*n2);
+// console.log(n1/n2);
+
+// Task -15
+
+// let n1="12345"
+// let n2="98765"
+// let netice=(n1*n2);
+// console.log(netice);
+// let number="1219253925"
+// console.log(number.charAt(6));
+
+
+// Task -17
+
+// let n1=Number(prompt("Dordreqemlli eded yaz"))
+// let n2=Number(prompt("Dordreqemlli eded yaz"))
+// console.log(n1+n2);
+
+// Task -18
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
