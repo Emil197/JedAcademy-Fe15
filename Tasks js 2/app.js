587553ad7d9ab7ecbea511ba2f54611dd273d806
Emil197 <qasimzadeemil71@gmail.com>
2023-06-17 16:55:25 +0400

    Tasks 2 js added
    Task 2 js

---
+++ Tasks js 2/app.js
@@ -0,0 +1,108 @@
+// Task -1
+
+// let ikireqemliEd=Number(prompt("Ikireqemli eded daxil edin"))
+
+// while (ikireqemliEd<=100) {
+
+//     console.log(ikireqemliEd);
+//     ikireqemliEd=ikireqemliEd+7;
+
+
+// }
+
+
+
+// Task -2
+
+
+// let N = Number(prompt("Bir ədəd daxil edin:"));
+
+// if (isNaN(N)) {
+//   N = Number(prompt("Düzgün bir ədəd daxil edin:"));
+// }
+
+// let count = 0;
+
+// while (count < N) {
+//   console.log("I know how to use cycles");
+//   count++;
+// }
+
+// Task -3
+
+
+// let num = 100;
+
+// while (num <= 999) {
+//   if (num % 10 === 0) {
+//     console.log(num);
+//   }
+//   num++;
+// }
+
+// Task -4
+
+// let numbers = 10
+// let toplam=0
+
+
+// while (numbers <= 99) {
+//     if (numbers %2 === 1) {
+//         toplam=numbers+toplam
+
+//         console.log(numbers);
+
+//     }
+//     console.log("Toplam: "+toplam);
+//     numbers++;
+// }
+
+
+
+// Task -5 ????
+
+// let number = Number(prompt("Ucreqemli eded daxil edin"))
+// let bolunen = 100
+// let toplam = 0
+// while (number <= 999 && number > 99) {
+//     if (number % (bolunen<=999) === 0) {
+//         toplam = toplam + bolunen;
+//         console.log(bolunen);
+
+//     }
+
+//     bolunen++;
+
+// }
+// console.log("bolunenlerin toplami : " + toplam);
+
+
+
+
+// Task -7
+
+// let vurma =1
+// for (let i = 10; i>=1; i--) {
+//     vurma=vurma*i
+//     console.log(i);
+//  }
+//  console.log("vurma neticesi : ", + vurma);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
